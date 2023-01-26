612c9f425be22939c68ec1f11484451662a83354
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-26 09:04:58 -0500

    Update

--- src/main/java/string_manipulation/GetCharacterAtGivenIndex.java
+++ src/main/java/string_manipulation/GetCharacterAtGivenIndex.java
@@ -1,7 +1,11 @@
 package string_manipulation;
+//get the character at the given index within the String
 
 public class GetCharacterAtGivenIndex {
     public static void main(String[] args) {
-        
+        String str = "Java";
+        int position = str.charAt(0);
+
+        System.out.println((char)position);
     }
 }
