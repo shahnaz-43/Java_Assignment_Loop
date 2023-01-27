350787b623921fcf17661732135d92e649b2e727
shahnaz-43 <kirei.kanan@gmail.com>
2023-01-27 16:47:24 -0500

    Upload

---
+++ src/main/java/java_basic/RandomNumInGivenRange.java
@@ -0,0 +1,8 @@
+package java_basic;
+
+public class RandomNumInGivenRange {
+    public static void main(String[] args) {
+        double x = Math.random();
+        System.out.println(x);
+    }
+}
