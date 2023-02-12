6ad85f2ad8eb08e7f0254fbddff3b2e1e043e05c
shahnaz-43 <kirei.kanan@gmail.com>
2023-02-11 23:55:42 -0500

    Upload

---
+++ src/main/java/ExceptionHandling/NumberFormatException.java
@@ -0,0 +1,11 @@
+package ExceptionHandling;
+
+public class NumberFormatException {
+    public static void main(String[] args) {
+        String s="welcome";
+        int n=Integer.parseInt(s);
+        System.out.println(n);
+    }
+}
+
+
