b1ad94ad10670ba29d124bd4ada6902d92056ba9
shahnaz-43 <kirei.kanan@gmail.com>
2023-02-11 23:55:50 -0500

    Upload

---
+++ src/main/java/ExceptionHandling/ArrayIndexOutOfBoundsException.java
@@ -0,0 +1,17 @@
+package ExceptionHandling;
+
+public class ArrayIndexOutOfBoundsException {
+    public static void main(String[] args) {
+        int[] a= {10,12,11};
+        System.out.println(a[10]);
+    }
+}
+/*
+public class ArrayIndexOutOfBoundsException {
+    public static void main(String[] args) {
+        int[] a= {10,12,11};
+        a[10]=100;
+
+    }
+}
+*/
